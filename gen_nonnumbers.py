da735643a2c8c01fa9b28540a9d9020da3341225
EgorovGA <egorov.ga@minc.tech>
2024-03-30 19:36:22 +0300

    make nonnumbers generator biased

--- gen_nonnumbers.py
+++ gen_nonnumbers.py
@@ -12,14 +12,22 @@ def random_datetime(start="2010-01-01 00:00:00", end="2020-12-31 23:59:59", time
     ptime = stime + prop * (etime - stime)
     return time.strftime(time_format, time.localtime(ptime))
 
+M = 16
 NAMES_F = ["Маша", "Ангелина", "Саша", "Яна", "Лиза", "Мора", "Эмми", "Белла", "Василисса", "Настя", "Оля", "Ира", "Чарли", "Кармилла", "Любовь", "Вера"]
 NAMES_M = ["Гордей", "Эрнест", "Саша", "Ян", "Лерой", "Морти", "Этан", "Бернард", "Василий", "Натан", "Аркадий", "Илон", "Аластор", "Энджел Даст", "Валентин", "Вокс"]
 CITIES = ["Москва", "Санкт-Петербург", "Обнинск", "Новосибирск", "Лас-Вегас", "Лос-Анджелес", "Нью-Йорк", "Вашингтон", "Ватикан", "Осло", "Томск", "Долгопрудный", "Пермь", "Орёл", "Париж", "Лондон"]
-DATES = [random_datetime() for i in range(16)]
+WEIGHTS = [M-i for i in range(M)]
+DATES = [random_datetime() for i in range(M)]
 
 N = 1000
 data = [["Муж", "Муж (город рождения)", "Жена", "Жена (город рождения)", "Дата путешествия"]] + [
-    [random.choice(NAMES_M), random.choice(CITIES), random.choice(NAMES_F), random.choice(CITIES), random.choice(DATES)] for i in range(N)
+    [
+        random.choices(NAMES_M, weights=WEIGHTS)[0],
+        random.choices(CITIES, weights=WEIGHTS)[0],
+        random.choices(NAMES_F, weights=WEIGHTS)[0],
+        random.choices(CITIES, weights=WEIGHTS)[0],
+        random.choices(DATES, weights=WEIGHTS)[0]
+    ] for i in range(N)
 ]
 data = data[:1] + [[NAMES_M[0], CITIES[0], NAMES_F[0], CITIES[1], DATES[0]]] + data[1:]
 Creator.xlsx(data, "data/data_random_nonnumbers.xlsx")
