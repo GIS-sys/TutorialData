30673539d75b5e170bb6fc7afa33c439ff50143d
EgorovGA <egorov.ga@minc.tech>
2024-03-30 18:30:58 +0300

    add folder data, new data file

--- creator.py
+++ creator.py
@@ -2,10 +2,21 @@ import openpyxl
 import random
 import string
 
+from pathlib import Path
+
 
 class Creator:
     @staticmethod
-    def xlsx(data, filename="data.xlsx"):
+    def folder(folder):
+        Path(folder).mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    def get_folder_name(path):
+        return Path(path).parent
+
+    @staticmethod
+    def xlsx(data, filename="data/data.xlsx"):
+        Creator.folder(Creator.get_folder_name(filename))
         wb = openpyxl.Workbook()
         sheet = wb.active
         for row, line in enumerate(data):
@@ -14,7 +25,8 @@ class Creator:
         wb.save(filename)
 
     @staticmethod
-    def csv(data, filename="data.csv", separator=","):
+    def csv(data, filename="data/data.csv", separator=","):
+        Creator.folder(Creator.get_folder_name(filename))
         with open(filename, "w") as f:
             for row, line in enumerate(data):
                 for col, cell in enumerate(line):
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from creator import Creator
+from decimal import Decimal, getcontext
+from math import factorial, sin
+import random
 
 
-data = [
-    ['x', 'y'],
-    [1, 2, 3],
-    ['a']
+def nth_pi_digit(n):
+    n += 2
+    getcontext().prec=n+1
+    t= Decimal(0)
+    pi = Decimal(0)
+    deno= Decimal(0)
+    k=0
+    t=(1)*(factorial(1))*(13591409+545140134*k)
+    deno = factorial(3*k)*(factorial(k)**3)*(640320**(3*k))
+    pi += Decimal(t)/Decimal(deno)
+    pi = pi * Decimal(12) / Decimal(640320 ** Decimal(1.5))
+    pi = 1/pi
+    return str(pi)[-2]
+
+
+N = 100
+data = [["Броски кубика", "Броски монетки", "Синус случайного аргумента", "Цифры числа PI"]] + [
+    [random.randint(1, 6), random.randint(0, 1), sin(random.random()), nth_pi_digit(i)] for i in range(N)
 ]
-Creator.xlsx(data, "simple_random.xlsx")
-Creator.csv(data, "simple_random.csv")
+Creator.csv(data, "data/data_random_numbers.csv")
+
+
+#Creator.xlsx(data, "data/simple_random.xlsx")
